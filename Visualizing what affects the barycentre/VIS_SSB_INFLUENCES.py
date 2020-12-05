ab1796e9ff7eedf43d846f755f743f7b882d4677
Anorak_Flynn <56981027+Quessian-Phaedrus-Sternabite@users.noreply.github.com>
2020-12-04 21:11:35 -0600

    Updating Barycentre influences

--- Visualizing what affects the barycentre/VIS_SSB_INFLUENCES.py
+++ Visualizing what affects the barycentre/VIS_SSB_INFLUENCES.py
@@ -6,3 +6,33 @@ import pandas as pd
 
 #LOAD THE KERNELS
 spiceypy.furnsh('kernel_meta.txt')
+
+#We want to compute miscellaneous positions w.r.t. the centre of
+#the Sun for a certain time interval.
+#First, we set an initial time in UTC
+INIT_TIME_UTC = datetime.datetime(year=2000, month=1, day=1, \
+                                  hour=0, minute=0, second=0)
+
+DELTA_DAYS = 10000
+END_TIME_UTC = INIT_TIME_UTC + datetime.timedelta(days=DELTA_DAYS)
+
+INIT_TIME_UTC_STR = INIT_TIME_UTC.strftime('%Y-%m-%d T%H:%M:%S')
+END_TIME_UTC_STR = END_TIME_UTC.strftime('%Y-%m-%d T%H:%M:%S')
+
+print('Init time in UTC: %s' % INIT_TIME_UTC_STR)
+print('End time in UTC: %s\n' % END_TIME_UTC_STR)
+
+INIT_TIME_ET = spiceypy.utc2et(INIT_TIME_UTC_STR)
+END_TIME_ET = spiceypy.utc2et(END_TIME_UTC_STR)
+
+#Create a numpy  array time interval
+TIME_INTERVAL_ET = np.linspace(INIT_TIME_ET, END_TIME_ET, DELTA_DAYS)
+
+# Using km is not intuitive. AU would scale it too severely. Since we compute
+# the Solar System Barycentre (SSB) w.r.t. the Sun; and since we expect it to
+# be close to the Sun, we scale the x, y, z component w.r.t the radius of the
+# Sun. We extract the Sun radii (x, y, z components of the Sun ellipsoid) and
+# use the x component
+_, RADII_SUN = spiceypy.bodvcd(bodyid=10, item='RADII', maxn=3)
+
+RADIUS_SUN = RADII_SUN[0]
