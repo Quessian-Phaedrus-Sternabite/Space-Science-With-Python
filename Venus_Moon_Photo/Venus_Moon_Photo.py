119108fb6d9791bf5f198ca01d81221ef7cd6225
Anorak_Flynn <56981027+Quessian-Phaedrus-Sternabite@users.noreply.github.com>
2020-12-06 15:41:40 -0600

    Almost done establishing dataframe and began computing phase angles. Ran into error: debugging.

--- Venus_Moon_Photo/Venus_Moon_Photo.py
+++ Venus_Moon_Photo/Venus_Moon_Photo.py
@@ -8,9 +8,9 @@ spiceypy.furnsh('kernel_meta.txt')
 
 
 #Create Initial and endtime date-time object as a string.
-INIT_TIME_UTC_STR = datetime.datetime(year=2020, month=8, day=1) \
+INIT_TIME_UTC_STR = datetime.datetime(year=2020, month=1, day=1) \
                         .strftime('%Y-%m-%dT%H:%M:%S')
-END_TIME_UTC_STR = datetime.datetime(year=2021, month=1, day=1) \
+END_TIME_UTC_STR = datetime.datetime(year=2020, month=6, day=1) \
                         .strftime('%Y-%m-%dT%H:%M:%S')
 
 #conver to Ephemeris Time (ET) using utc2et
@@ -26,4 +26,44 @@ TIME_INTERVAL_ET = np.arange(INIT_TIME_ET, END_TIME_ET, DELTA_HOUR_IN_SECONDS)
 INNER_SOLSYS_DF = pd.DataFrame()
 
 #Set the ET column thatg stors all ET
-INNER_SOLSYS_DF.loc[:, 'ET']
+INNER_SOLSYS_DF.loc[:, 'ET'] = TIME_INTERVAL_ET
+
+# The column UTC transforms all ETs back to a UTC format. The function
+# spicepy.et2datetime is NOT an official part of SPICE (there you can find
+# et2utc).
+# However this function returns immediately a date-time object
+INNER_SOLSYS_DF.loc[:, 'UTC'] = \
+    INNER_SOLSYS_DF['ET'].apply(lambda x: spiceypy.et2datetime(et=x))
+
+# Compute now the phase angle between Venus and Sun as seen from Earth
+#
+# For this computation we need the SPICE function phaseq. et is the ET. Based
+# on SPICE's logic the target is the Earth (399) and the illumination source
+# (illmn) is the Sun (10), the observer (obsrvr) is Venus with the ID 299.
+# We apply a correction that considers the movement of the planets and the
+# light time (LT+S)
+INNER_SOLSYS_DF.loc[:, 'EARTH_VEN2SUN_ANGLE'] = \
+    INNER_SOLSYS_DF['ET'].apply(lambda x: #
+                                    np.degrees(spiceypy.phaseq(et=x, \
+                                                               target='399', \
+                                                               illmn='10', \
+                                                               obsrvr='299', \
+                                                               abcorr='LT+S')))
+
+#Compute the angle between the Moon and the sun. We apply the same
+#Function (phase1. The Moon NAIF ID is 301
+INNER_SOLSYS_DF[:, 'EARTH_MOON2SUN_ANGLE'] = \
+    INNER_SOLSYS_DF['ET'].apply(lambda x: \
+                                    np.degrees(spiceypy.phaseq(et=x, \
+                                                               target='399', \
+                                                               illmn='10', \
+                                                               obsrvr='301', \
+                                                               abcorr='LT+S')))
+#COmpute finally the phase angle between the Moon and Venus
+INNER_SOLSYS_DF.loc[:, 'EARTH_MOON2VEN_ANGLE'] = \
+    INNER_SOLSYS_DF['ET'].apply(lambda x: \
+                                np.degrees(spiceypy.phaseq(et=x, \
+                                                           target='399', \
+                                                           illmn='299', \
+                                                           obsrvr='301', \
+                                                           abcorr='LT+S')))
