2f57bca300446a66b2e428bdd8f08d49ab26b311
Anorak_Flynn <56981027+Quessian-Phaedrus-Sternabite@users.noreply.github.com>
2020-12-06 15:30:11 -0600

    Establishing pandas dataframe.

--- Venus_Moon_Photo/Venus_Moon_Photo.py
+++ Venus_Moon_Photo/Venus_Moon_Photo.py
@@ -14,8 +14,16 @@ END_TIME_UTC_STR = datetime.datetime(year=2021, month=1, day=1) \
                         .strftime('%Y-%m-%dT%H:%M:%S')
 
 #conver to Ephemeris Time (ET) using utc2et
-INT_TIME_ET = spiceypy.utc2et(INIT_TIME_UTC_STR)
+INIT_TIME_ET = spiceypy.utc2et(INIT_TIME_UTC_STR)
 END_TIME_ET = spiceypy.utc2et(END_TIME_UTC_STR)
 
 #Set the number of seconds per hour. This value is used to compute
 #The phase angles in one hour steps.
+DELTA_HOUR_IN_SECONDS = 3600.0
+TIME_INTERVAL_ET = np.arange(INIT_TIME_ET, END_TIME_ET, DELTA_HOUR_IN_SECONDS)
+
+#Create a pandas dataframe to store computed parameters, positions, etc.
+INNER_SOLSYS_DF = pd.DataFrame()
+
+#Set the ET column thatg stors all ET
+INNER_SOLSYS_DF.loc[:, 'ET']
