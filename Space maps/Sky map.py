c8881607ccb902b20242ac5bee2fa0a49dc75609
Anorak_Flynn <56981027+Quessian-Phaedrus-Sternabite@users.noreply.github.com>
2020-12-07 08:57:05 -0600

    Created pandas dataframe and began establishing calculations.

--- Space maps/Sky map.py
+++ Space maps/Sky map.py
@@ -11,3 +11,31 @@ DATETIME_UTC = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
 
 #convert to ET
 DATETIME_ET = spiceypy.utc2et(DATETIME_UTC)
+
+# We want to compute the coordinates for different Solar System bodies as seen
+# from our planet. First, a pandas dataframe is set that is used to append the
+# computed data
+solsys_df = pd.DataFrame()
+
+# Add the ET and the corresponding UTC date-time string
+solsys_df.loc[:, 'ET'] = [DATETIME_ET]
+solsys_df.loc[:, 'UTC'] = [DATETIME_UTC]
+
+# Set a dictionary that lists some body names and the corresponding NAIF ID
+# code. Mars has the ID 499, however the loaded kernels do not contain the
+# positional information. We use the Mars barycentre instead
+SOLSYS_DICT = {'SUN': 10, 'VENUS': 299, 'MOON': 301, 'MARS': 4}
+
+#Iterate through the dictionary and compute miscellaneous positional
+#Parameters
+for body_name in SOLSYS_DICT:
+
+    # First, compute the directional vector Earth - body in ECLIPJ2000. Use
+    # LT+S light time correction. spkezp returns the directional vector and
+    # light time. Apply [0] to get only the vector
+    solsys_df.loc[:, f'dir_{body_name}_wrt_earth_ecl'] = solsys_df['ET'] \
+        .apply(lambda x: spiceypy.spkezp(targ=SOLSYS_DICT[body_name], \
+                                         et=x, \
+                                         ref='ECLIPJ2000', \
+                                         abcorr='LT+S', \
+                                         obs=399)[0])
