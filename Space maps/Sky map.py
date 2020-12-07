f62a1eabf0f36c4023cf2d252257889dac86f8dc
Anorak_Flynn <56981027+Quessian-Phaedrus-Sternabite@users.noreply.github.com>
2020-12-07 09:01:18 -0600

    Minor debugging - fixed all errors. Finished converting latitde and longitude to radians and stored in DataFrame.

--- Space maps/Sky map.py
+++ Space maps/Sky map.py
@@ -39,3 +39,15 @@ for body_name in SOLSYS_DICT:
                                          ref='ECLIPJ2000', \
                                          abcorr='LT+S', \
                                          obs=399)[0])
+
+    # Compute the longitude and latitude of the body in radians in ECLIPJ2000
+    # using the function recrad. recrad returns the distance, longitude and
+    # latitude value; thus, apply [1] and [2] to get the longitude and
+    # latitude, respectively
+    solsys_df.loc[:, f'{body_name}_long_rad_ecl'] = \
+        solsys_df[f'dir_{body_name}_wrt_earth_ecl'] \
+        .apply(lambda x: spiceypy.recrad(x)[1])
+
+    solsys_df.loc[:, f'{body_name}_lat_rad_ecl'] = \
+        solsys_df[f'dir_{body_name}_wrt_earth_ecl'] \
+        .apply(lambda x: spiceypy.recrad(x)[2])
