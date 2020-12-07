7e7bc408d0dbab484e5c44f88f7eba176dbd1272
Anorak_Flynn <56981027+Quessian-Phaedrus-Sternabite@users.noreply.github.com>
2020-12-07 09:10:58 -0600

    Computed long into radians; I'm testing now.

--- Space maps/Sky map.py
+++ Space maps/Sky map.py
@@ -6,10 +6,10 @@ from matplotlib import pyplot as plt
 
 spiceypy.furnsh('kernel_meta.txt')
 
-#Create an initial datetime object that is converted into a string
+# Create an initial datetime object that is converted into a string
 DATETIME_UTC = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
 
-#convert to ET
+# convert to ET
 DATETIME_ET = spiceypy.utc2et(DATETIME_UTC)
 
 # We want to compute the coordinates for different Solar System bodies as seen
@@ -26,10 +26,9 @@ solsys_df.loc[:, 'UTC'] = [DATETIME_UTC]
 # positional information. We use the Mars barycentre instead
 SOLSYS_DICT = {'SUN': 10, 'VENUS': 299, 'MOON': 301, 'MARS': 4}
 
-#Iterate through the dictionary and compute miscellaneous positional
-#Parameters
+# Iterate through the dictionary and compute miscellaneous positional
+# Parameters
 for body_name in SOLSYS_DICT:
-
     # First, compute the directional vector Earth - body in ECLIPJ2000. Use
     # LT+S light time correction. spkezp returns the directional vector and
     # light time. Apply [0] to get only the vector
@@ -46,10 +45,19 @@ for body_name in SOLSYS_DICT:
     # latitude, respectively
     solsys_df.loc[:, f'{body_name}_long_rad_ecl'] = \
         solsys_df[f'dir_{body_name}_wrt_earth_ecl'] \
-        .apply(lambda x: spiceypy.recrad(x)[1])
+            .apply(lambda x: spiceypy.recrad(x)[1])
 
     solsys_df.loc[:, f'{body_name}_lat_rad_ecl'] = \
         solsys_df[f'dir_{body_name}_wrt_earth_ecl'] \
-        .apply(lambda x: spiceypy.recrad(x)[2])
+            .apply(lambda x: spiceypy.recrad(x)[2])
 
-    
+# Before we plot the data, we need to convert the longitude data into a
+# matplotlib compatible format. We computed longitude values between 0 and
+# 2*pi (360 degrees). matplotlib expects values between -pi and +pi. Further,
+# sky maps count from 0 degrees longitude to the left. Thus we need also to
+# invert the longitude values
+for body_name in SOLSYS_DICT:
+    solsys_df.loc[:, f'{body_name}_long_rad4plot_ecl'] = \
+        solsys_df[f'{body_name}_long_rad_ecl'] \
+            .apply(lambda x: -1*((x % np.pi) -np.pi) if x > np.pi \
+                    else -1*x)
