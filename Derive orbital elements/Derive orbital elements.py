1fa1d9ec68660e29f77e435481924e95e7bff10f
Ian <kelley.ianscott@gmail.com>
2021-01-28 12:26:18 -0600

    Fixed error in which it returned a negative 0. Solved by adding 0.

--- Derive orbital elements/Derive orbital elements.py
+++ Derive orbital elements/Derive orbital elements.py
@@ -9,3 +9,16 @@ DATETIME_UTC = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
 # Convert to Ephemeris Time (ET) using the SPICE function utc2et
 DATETIME_ET = spiceypy.utc2et(DATETIME_UTC)
 
+# ECLIPJ2000_DE405 and ECLIPJ2000 appear to be similar?! A transformation
+# matrix between both coordinate systems (for state vectors) should be
+# consequently the identity matrix
+MAT = spiceypy.sxform(instring='ECLIPJ2000_DE405', \
+                      tostring='ECLIPJ2000', \
+                      et=DATETIME_ET)
+
+# Let's print the transformation matrix row-wise (spoiler alert: it is the
+# identity matrix)
+print('Transformation matrix between ECLIPJ000_DE405 and ECLIPJ2000')
+for mat_row in MAT:
+    print(f'{(np.round(mat_row, 2) + 0)}')
+print('\n')
