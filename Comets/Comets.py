1b6e09fe9e88c7e0d28b6397bc1b04341bb18315
Quess <kelley.ianscott@gmail.com>
2021-02-26 22:58:26 -0600

    Converted the time from the downloaded file to UTC, and then ET
    Fixed minor error that occurred by forgetting to specify EPOCH_UTC on line 66's lambda, as well as added approximately 40 lines.

--- Comets/Comets.py
+++ Comets/Comets.py
@@ -24,3 +24,43 @@ func.download_file(DL_PATH, DL_URL)
 
 # Load the SPICE kernel meta file
 spiceypy.furnsh('kernel_meta.txt')
+
+# Read the g-zipped json file with pandas read_json. The function allows one
+# to read compressed data
+c_df = pd.read_json('raw_data/cometels.json.gz', compression='gzip')
+
+# First we parse the date and time information. The dataset contains two
+# time related information: the date-time of the last perihelion passage and
+# another variable called Epoch. However, "epoch" is not related to the mean
+# anomaly related epoch and represents other time information in this case.
+#
+# For our "actual" Epoch case we need to create a UTC time string based on the
+# date and time of the last perihelion passage (the time corresponds to a mean
+# anomaly of 0 degrees). The Day is given in DAY.FRACTION_OF_DAY. We extract
+# only the day
+c_df.loc[:, 'EPOCH_UTC_DATE'] = \
+    c_df.apply(lambda x: str(x['Year_of_perihelion']) + '-'
+                         + str(x['Month_of_perihelion']) + '-'
+                         + str(x['Day_of_perihelion']).split('.')[0],
+               axis=1)
+
+# Now we need to parse the .FRACTION_OF_DAY given between (0.0, 1.0). First,
+# Create a place-holder date
+PRE_TIME = datetime.datetime(year=2000, month=1, day=1)
+
+# Use the pre_time date-time object and add the days and fraction of days with
+# the timedelta function from the datetime library. Extract only the time
+# substring ...
+c_df.loc[:, 'EPOCH_UTC_TIME'] = \
+    c_df['Day_of_perihelion'] \
+        .apply(lambda x: (PRE_TIME + datetime.timedelta(days=x)).
+               strftime('%H:%M:%S'))
+
+# ... and based with the date, create now the UTC date-time
+c_df.loc[:, 'EPOCH_UTC'] = c_df.apply(lambda x: x['EPOCH_UTC_DATE']
+                                                + 'T'
+                                                + x['EPOCH_UTC_TIME'],
+                                      axis=1)
+
+# Convert the UTC datetieme to ET
+c_df.loc[:, 'EPOCH_ET'] = c_df['EPOCH_UTC'].apply(lambda x: spiceypy.utc2et(x))
