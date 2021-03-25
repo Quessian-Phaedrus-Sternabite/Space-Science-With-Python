3f4a12c11eb38d934804e19496e5b36f5c762810
Quess <kelley.ianscott@gmail.com>
2021-03-25 08:26:49 -0500

    Added meta kernel and created PD dataframes

--- Jupiter Rendezvous/Jupiter Rendezvous.py
+++ Jupiter Rendezvous/Jupiter Rendezvous.py
@@ -11,3 +11,24 @@ from matplotlib import pyplot as plt
 
 # Import scipy for the Kernel Density Estimator functionality
 from scipy import stats
+
+# Connect to the comet database established in comets.py.
+# It is uploaded on github.
+con = sqlite3.connect('../_databases/_comets/mpc_comets.db')
+
+# Set a cursor
+cur = con.cursor()
+
+# Create a pandas dataframe that contians the name of the comer (needed later),
+# the semi-major axis, inclination and eccentricity
+# for P type ...
+P_TYPE_DF = pd.read_sql('SELECT NAME, SEMI_MAJOR_AXIS_AU, INCLINATION_DEG,'
+                        'ECCENTRICITY FROM comets_main WHERE ORBIT_TYPE="P"',
+                        con)
+
+# ... and C type comics. For this type: set the eccentricity smaller 1 (bound
+# Orbits
+C_TYPE_DF = pd.read_sql('SELECT NAME, SEMI_MAJOR_AXIS_AU, INCLINATION_DEG,'
+                        'ECCENTRICITY FROM comets_main WHERE ORBIT_TYPE="C"'
+                        'AND ECCENTRICITY<1', con)
+
