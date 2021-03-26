a8f2fda41c759fe0ed66523de2269cc0b316aa1e
Quess <kelley.ianscott@gmail.com>
2021-03-26 09:32:02 -0500

    Added A, made the info correct.

--- Comet Info/Comet Info.py
+++ Comet Info/Comet Info.py
@@ -16,6 +16,11 @@ C_TYPE_DF = pd.read_sql('SELECT PERIHELION_AU, SEMI_MAJOR_AXIS_AU, APHELION_AU, 
                         'EPOCH_ET, ABSOLUTE_MAGNITUDE, SLOPE_PARAMETER, TISSERAND_JUP FROM comets_main WHERE '
                         'ORBIT_TYPE="C"', con)
 
+A_TYPE_DF = pd.read_sql('SELECT PERIHELION_AU, SEMI_MAJOR_AXIS_AU, APHELION_AU, INCLINATION_DEG,'
+                        'ECCENTRICITY, ARG_OF_PERIH_DEG, LONG_OF_ASC_NODE_DEG, MEAN_ANOMALY_DEG,'
+                        'EPOCH_ET, ABSOLUTE_MAGNITUDE, SLOPE_PARAMETER, TISSERAND_JUP FROM comets_main WHERE '
+                        'ORBIT_TYPE="A"', con)
+
 name = \
     ["PERIHELION_AU",
      "SEMI_MAJOR_AXIS_AU",
@@ -32,9 +37,11 @@ name = \
 
 P_INFO = []
 C_INFO = []
+A_INFO = []
 for i in name:
     P_INFO.append(P_TYPE_DF[i])
-    C_INFO.append(P_TYPE_DF[i])
+    C_INFO.append(C_TYPE_DF[i])
+    A_INFO.append(A_TYPE_DF[i])
 
 x = 0
 for i in P_INFO:
@@ -50,6 +57,16 @@ print("---------------------------------------------------------------")
 
 x = 0
 for i in C_INFO:
+    print(name[x])
+    print(f'The mean of {name[x]} - C is {i.mean()}')
+    print(f'The median of {name[x]} - C is {i.median()}')
+    print(f'The maximum of {name[x]} - C is {i.max()}')
+    print(f'The minimum of {name[x]} - C is {i.min()}')
+    x += 1
+    print("\n")
+
+x = 0
+for i in A_INFO:
     print(name[x])
     print(f'The mean of {name[x]} - C is {i.mean()}')
     print(f'The median of {name[x]} - C is {i.median()}')
